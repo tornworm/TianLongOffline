46d47013847c45b4561a233e8b61d1862d673504
quanquan <869003106@qq.com>
2019-06-06 19:04:09 +0800

    宠物-技能资质
    宠物-技能资质

---
+++ Client/TianLong1/Assets/Work/Work7/Pet_ZiZhi.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet_ZiZhi : MonoBehaviour {
+
+    public GameObject Pet;
+	// Use this for initialization
+	void Start () {
+       
+
+        for(int i = 0; i < 12; i++)
+        {
+            GameObject obj = Instantiate(Pet, transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
