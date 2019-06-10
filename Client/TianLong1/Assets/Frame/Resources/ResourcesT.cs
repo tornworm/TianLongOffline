f1fd52b3aa35233fec3b6478052523a172a3b201
tornworm <42328527+tornworm@users.noreply.github.com>
2019-06-10 15:19:58 +0800

    1
    1

--- Client/TianLong1/Assets/Frame/Resources/ResourcesT.cs
+++ Client/TianLong1/Assets/Frame/Resources/ResourcesT.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class ResourcesT  {
 
-    public static void Load(string path)
+    public static Object Load(string path)
     {
-        Resources.Load(path);
+       return  Resources.Load(path);
     }
-  
+    public static T Load<T>(string path) where T : Object
+    {
+        return (T)Resources.Load(path, typeof(T));
+    }
+
 }
 
