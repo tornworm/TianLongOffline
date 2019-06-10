8cc6c9654194c4efd46e61b0542d81d519227151
wangyanyunisme <51374388+wangyanyunisme@users.noreply.github.com>
2019-06-11 00:19:52 +0800

    1
    1

---
+++ Client/TianLong1/Assets/Scripts/UI/Bag/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item { 
+
+    public string name { get; set; }
+    public int gold { get; set; }
+    public string image { get; set; }
+
+    public Item(string name,int gold)
+    {
+        this.name = name;
+        this.gold = gold;
+        image = "Textures/UI/Normal/common_001";
+         
+    }
+}
---
+++ Client/TianLong1/Assets/Scripts/UI/Store/StoreManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreManager {
+
+    private static StoreManager singleton;
+    public static StoreManager Singleton
+    {
+        get
+        {
+            return singleton ?? (singleton = new StoreManager());
+        }
+    }
+    private StoreManager() { }
+
+
+}
